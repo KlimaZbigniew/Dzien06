a98a9e92c9e708242506655cec37ed13b3cae430
kurs <kurs@DESKTOP-V3HDKPQ>
2021-08-01 16:02:41 +0200

    MySQL Connection

---
+++ MySQLConnect/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace MySQLConnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tbDBName = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbSQL = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.lvGrid = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(112, 25);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(100, 20);
+            this.tbHost.TabIndex = 0;
+            this.tbHost.Text = "127.0.0.1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // tbDBName
+            // 
+            this.tbDBName.Location = new System.Drawing.Point(112, 103);
+            this.tbDBName.Name = "tbDBName";
+            this.tbDBName.Size = new System.Drawing.Size(100, 20);
+            this.tbDBName.TabIndex = 2;
+            this.tbDBName.Text = "classicmodels";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(112, 77);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 3;
+            this.tbPassword.Text = "MariaDB2021";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(112, 51);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(100, 20);
+            this.tbUser.TabIndex = 4;
+            this.tbUser.Text = "root";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(112, 122);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Połącz";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbSQL
+            // 
+            this.tbSQL.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbSQL.Location = new System.Drawing.Point(229, 25);
+            this.tbSQL.Multiline = true;
+            this.tbSQL.Name = "tbSQL";
+            this.tbSQL.Size = new System.Drawing.Size(445, 104);
+            this.tbSQL.TabIndex = 7;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(680, 26);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(108, 103);
+            this.btnRun.TabIndex = 8;
+            this.btnRun.Text = "Wykonaj zapytanie";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // lvGrid
+            // 
+            this.lvGrid.HideSelection = false;
+            this.lvGrid.Location = new System.Drawing.Point(112, 151);
+            this.lvGrid.Name = "lvGrid";
+            this.lvGrid.Size = new System.Drawing.Size(676, 234);
+            this.lvGrid.TabIndex = 9;
+            this.lvGrid.UseCompatibleStateImageBehavior = false;
+            this.lvGrid.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvGrid);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tbSQL);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbDBName);
+            this.Controls.Add(this.tbHost);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox tbDBName;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbSQL;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.ListView lvGrid;
+    }
+}
+
---
+++ MySQLConnect/Form1.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySQLConnect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection conn = null;
+        bool isOpen = false;
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string cs = $"server={tbHost.Text};userid={tbUser.Text};password={tbPassword.Text};database={tbDBName.Text};port=3306;sslMode=None";
+
+            try
+            {
+                if (isOpen)
+                {
+                    conn.Close();
+                    conn.Dispose();
+                    isOpen = false;
+                    btnConnect.Text = "Połącz";
+                }
+                else
+                {
+                    conn = new MySqlConnection(cs);
+                    conn.Open();
+                    isOpen = true;
+                    btnConnect.Text = "Rozłącz";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Błąd połączenia z DB:\n\n\n\n\n {ex.Message}");
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (!isOpen)
+                return;
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(tbSQL.Text,conn);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Błąd pobrania danych:\n\n {ex.Message}");
+            }
+        }
+    }
+}
